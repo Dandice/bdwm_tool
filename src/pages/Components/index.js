ac55c73e6b4d1332bac1c6030eabd646f1023dc4
Dandice <544483418@qq.com>
2016-10-12 20:24:03 +0800

    code dev

---
+++ src/pages/Components/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Components = React.createClass({
+    getInitialState(){
+        return {
+        };
+    },
+    componentDidMount(){
+    },
+    render(){
+        return <div className="outArticle_detail_out">
+            xxxx
+        </div>
+    }
+})
+
+export default Components
